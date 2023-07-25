6ff0ae5af983c2bd31fdb64e0f601f8948e14409
Natel77 <damte7@outlook.com>
2023-07-24 22:32:22 -0700

    blank lline

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,6 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 	if (n == 0)
 		return (0);
+
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
@@ -19,4 +20,5 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	va_end(valist);
 	return (sum);
+
 }
